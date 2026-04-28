b529de6e65c131ab26376bd6a174c0314027ee2d
HeadlesssS <aaryanarju@gmail.com>
2026-04-28 10:38:31 +0545

    Initial setup:Database and css

---
+++ Backend/Ser_Viscar/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ser_Viscar.Models;
+namespace Ser_Viscar.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Backend/Ser_Viscar/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Ser_Viscar.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
